4d6e6a9d869b7bc605557d831a52e6f0f1c4ef2e
Harrison Hough <harrisonhough4@gmail.com>
2018-06-25 22:12:47 +1000

    Project Setup
    Initial Project Setup
    Scene staging and basic scripting

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public BirdState birdState { get; set; }
+
+    private float delayDestroyTime = 2f;
+
+    private LineRenderer lineRenderer;
+    private Rigidbody2D myRigidbody;
+    private CircleCollider2D myCollider;
+    private AudioSource audioSource;
+
+    private bool waitingToDestroy = false;
+	// Use this for initialization
+	void Awake () {
+
+        InitializeVariables();
+	}
+
+    private void FixedUpdate()
+    {
+        if (birdState == BirdState.Thrown && myRigidbody.velocity.sqrMagnitude <= GameVariables.MinVelocity) {
+            if (!waitingToDestroy)
+            {
+                waitingToDestroy = true;
+                StartCoroutine(DestroyAfterDelay(delayDestroyTime));
+            }
+        }
+    }
+
+    void InitializeVariables()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<CircleCollider2D>();
+        audioSource = GetComponent<AudioSource>();
+
+        lineRenderer.enabled = false;
+        lineRenderer.sortingLayerName = "Foreground";
+
+        myRigidbody.isKinematic = true;
+        myCollider.radius = GameVariables.BirdColliderRadiusBig;
+
+        birdState = BirdState.BeforeThrown;
+    }
+
+    public void OnThrow()
+    {
+        audioSource.Play();
+        lineRenderer.enabled = true;
+        myRigidbody.isKinematic = false;
+        myCollider.radius = GameVariables.BirdColliderRadiusNormal;
+        birdState = BirdState.Thrown;
+    }
+
+    IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private float health = 70f;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.GetComponent<Rigidbody2D>() == null)
+            return;
+
+        float damage = target.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10f;
+
+        if (damage > 10)
+        {
+            audioSource.Play();
+        }
+
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+}
