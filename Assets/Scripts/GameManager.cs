7df34c413987366d4720ebe245dbe5e3a812c5c7
Harrison Hough <harrisonhough4@gmail.com>
2018-06-26 22:39:49 +1000

    Added features
    -Slingshot logic
    - Game Manager logic

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public CameraFollow cameraFollow;
+
+    int currentBirdIndex;
+
+    public Slingshot slingshot;
+
+    [HideInInspector]
+    public static GameState gamestate;
+
+    private List<GameObject> bricks;
+    private List<GameObject> birds;
+    private List<GameObject> pigs;
+
+    // Use this for initialization
+    void Awake () {
+        gamestate = GameState.Start;
+        slingshot.enabled = false;
+
+        //TODO optimize later
+        bricks = new List<GameObject>(GameObject.FindGameObjectsWithTag("Brick"));
+        birds = new List<GameObject>(GameObject.FindGameObjectsWithTag("Bird"));
+        pigs = new List<GameObject>(GameObject.FindGameObjectsWithTag("Pig"));
+    }
+
+    void OnEnable()
+    {
+        //TODO double check for bugs
+        slingshot.birdthrown += SlingshotBirdThrown;
+    }
+
+    private void OnDisable()
+    {
+        //TODO double check for bugs
+        slingshot.birdthrown -= SlingshotBirdThrown;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        switch (gamestate)
+        {
+            case GameState.Start:
+                if (Input.GetMouseButtonUp(0))
+                {
+                    AnimateBirdToSlingshot();
+                }
+                break;
+            case GameState.Playing:
+                //TODO Simplify if possible
+                if (slingshot.slingshotState == SlingshotState.BirdFlying 
+                    && (BricksBirdsPigsStoppedMoving() || Time.time - slingshot.timeSinceThrown > 5f))
+                {
+                    slingshot.enabled = false;
+                    AnimateCameraToStartPosition();
+                    gamestate = GameState.BirdMovingToSlingshot;
+
+                }
+                break;
+            case GameState.Won:
+            case GameState.Lost:
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //TODO cleanup / upgrade
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                    //Application.LoadLevel(Application.loadedLevelName);
+                }
+                break;
+
+        }
+	}
+
+    void AnimateBirdToSlingshot()
+    {
+        gamestate = GameState.BirdMovingToSlingshot;
+
+        //TODO clean this up
+        birds[currentBirdIndex].transform.positionTo(
+            Vector2.Distance(birds[currentBirdIndex].transform.position / 10,
+            slingshot.birdWaitPosition.position) / 10, slingshot.birdWaitPosition.position).
+            setOnCompleteHandler((x)=> {
+                x.complete();
+                x.destroy();
+
+                gamestate = GameState.Playing;
+                slingshot.enabled = true;
+
+                slingshot.birdToThrow = birds[currentBirdIndex];
+            });
+    }
+
+    bool BricksBirdsPigsStoppedMoving()
+    {
+        //TODO clean up this mess
+        foreach (var item in bricks.Union(birds).Union(pigs))
+        {
+            if (item != null && item.GetComponent<Rigidbody2D>().velocity.sqrMagnitude > GameVariables.MinVelocity) {
+                return false;
+            } 
+        }
+        return true;
+    }
+
+    private bool AllPigsAreDestroyed()
+    {
+        //TODO Clean up
+        return pigs.All(x => x == null);
+    }
+
+    private void AnimateCameraToStartPosition()
+    {
+        float duration = Vector2.Distance(Camera.main.transform.position, cameraFollow.startingPosition) / 10f;
+        if (duration == 0.0f)
+            duration = 0.1f;
+
+        //TODO Cleanup
+        Camera.main.transform.positionTo(duration, 
+            cameraFollow.startingPosition).setOnBeginHandler((x) => 
+            {
+                cameraFollow.isFollowing = false;
+                if (AllPigsAreDestroyed())
+                {
+                    gamestate = GameState.Won;
+                }
+                else if (currentBirdIndex == birds.Count - 1)
+                {
+                    gamestate = GameState.Lost;
+                }
+                else
+                {
+                    slingshot.slingshotState = SlingshotState.Idle;
+                    currentBirdIndex++;
+                    AnimateBirdToSlingshot();
+                }
+            }
+            );
+    }
+
+    private void SlingshotBirdThrown()
+    {
+        cameraFollow.birdToFollow = birds[currentBirdIndex].transform;
+        cameraFollow.isFollowing = true;
+    }
+}
--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -24,23 +24,154 @@ public class Slingshot : MonoBehaviour {
     public float timeSinceThrown;
 
     public delegate void BirdThrown();
-    public event BirdThrown birdthron;
+    public event BirdThrown birdthrown;
 
-	
-	void Awake () {
+
+    void Awake() {
         slingshotLineRenderer1.sortingLayerName = "Foreground";
         slingshotLineRenderer2.sortingLayerName = "Foreground";
         trajectoryLineRenderer.sortingLayerName = "Foreground";
 
         slingshotState = SlingshotState.Idle;
-        slingshotLineRenderer1.SetPosition(0,leftSlingshotOrigin.position);
+        slingshotLineRenderer1.SetPosition(0, leftSlingshotOrigin.position);
         slingshotLineRenderer2.SetPosition(0, rightSlingshotOrigin.position);
 
-        slingshotMiddleVector = new Vector3((leftSlingshotOrigin.position.x + rightSlingshotOrigin.position.x)/2, (leftSlingshotOrigin.position.y + rightSlingshotOrigin.position.y)/2, 0);
+        slingshotMiddleVector = new Vector3((leftSlingshotOrigin.position.x + rightSlingshotOrigin.position.x) / 2, (leftSlingshotOrigin.position.y + rightSlingshotOrigin.position.y) / 2, 0);
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+        switch (slingshotState)
+        {
+            case SlingshotState.Idle:
+
+                InitializeBird();
+
+                DisplaySlingShotLineRenderers();
+
+                //TODO fix for performance
+                if (Input.GetMouseButton(0))
+                {
+                    Vector3 location = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                    if (birdToThrow.GetComponent<CircleCollider2D>() == Physics2D.OverlapPoint(location))
+                    {
+                        slingshotState = SlingshotState.UserPulling;
+                    }
+                }
+                break;
+            case SlingshotState.UserPulling:
+
+                DisplaySlingShotLineRenderers();
+
+                if (Input.GetMouseButton(0))
+                {
+                    Vector3 location = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    location.z = 0;
+
+                    if (Vector3.Distance(location, slingshotMiddleVector) > 1.5f)
+                    {
+                        var maxPosition = (location - slingshotMiddleVector).normalized * 1.5f + slingshotMiddleVector;
+                        birdToThrow.transform.position = maxPosition;
+                    }
+                    else
+                    {
+                        birdToThrow.transform.position = location;
+                    }
+                    var distance = Vector3.Distance(slingshotMiddleVector, birdToThrow.transform.position);
+                    DisplayTrajectoryLineRenderer(distance);
+                }
+                else //No Tap
+                {
+                    SetTrajectoryLineRendererActive(true);
+                    timeSinceThrown = Time.time;
+
+                    float distance = Vector3.Distance(slingshotMiddleVector, birdToThrow.transform.position);
+                    if (distance > 1)
+                    {
+                        SetSlingshotLineRenderer(false);
+                        slingshotState = SlingshotState.BirdFlying;
+
+                        ThrowBird(distance);
+                    }
+                    else
+                    {
+                        birdToThrow.transform.positionTo(distance / 10, birdWaitPosition.position);
+                        InitializeBird();
+                    }
+
+
+                }
+                break;
+
+        }
+    }
+
+    private void InitializeBird()
+    {
+        birdToThrow.transform.position = birdWaitPosition.position;
+        slingshotState = SlingshotState.Idle;
+        SetSlingshotLineRenderer(true);
+    }
+
+    void SetSlingshotLineRenderer(bool active)
+    {
+        slingshotLineRenderer1.enabled = active;
+        slingshotLineRenderer2.enabled = active;
+    }
+
+    void DisplaySlingShotLineRenderers()
+    {
+        slingshotLineRenderer1.SetPosition(1, birdToThrow.transform.position);
+        slingshotLineRenderer2.SetPosition(1, birdToThrow.transform.position);
+
+    }
+
+    void SetTrajectoryLineRendererActive(bool active)
+    {
+        trajectoryLineRenderer.enabled = active;
+    }
+
+    void DisplayTrajectoryLineRenderer(float distance)
+    {
+        SetTrajectoryLineRendererActive(true);
+
+        Vector3 v = slingshotMiddleVector - birdToThrow.transform.position;
+        int segmentCount = 25;
+
+        Vector2[] segments = new Vector2[segmentCount];
+
+        segments[0] = birdToThrow.transform.position;
+
+        Vector2 segVelocity = new Vector2(v.x, v.y) * throwSpeed * distance;
+
+        for (int i = 1; i < segmentCount; i++)
+        {
+            float time = i * Time.fixedDeltaTime * 5f;
+            segments[i] = segments[0] + segVelocity * time + 0.5f * Physics2D.gravity * Mathf.Pow(time, 2);
+        }
+
+        //CHECK
+        //trajectoryLineRenderer.SetVertexCount( segmentCount);
+        trajectoryLineRenderer.positionCount = segmentCount;
+        for (int i = 0; i < segmentCount; i++)
+        {
+            trajectoryLineRenderer.SetPosition(i, segments[i]);
+        }
+    }
+
+    private void ThrowBird(float distance)
+    {
+        Vector3 velocity = slingshotMiddleVector - birdToThrow.transform.position;
+
+        birdToThrow.GetComponent<Bird>().OnThrow();
+
+        //TODO move to bird if possible
+        birdToThrow.GetComponent<Rigidbody>().velocity = new Vector2(velocity.x, velocity.y) * throwSpeed * distance;
+
+        if (birdthrown != null)
+            birdthrown();
+    }
+
+
 }
